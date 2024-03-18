eb7baa736eaea077e6029abd270104211829da90
samuel.documet <samuel.documet@pagonxt.com>
2024-03-18 23:25:40 +0100

    feat(0000): refactor code for readability

--- projects/web-app/src/app/app.component.ts
+++ projects/web-app/src/app/app.component.ts
@@ -1,7 +1,7 @@
 /**
  * @anuglar imports
  * **/
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, effect, ElementRef, ViewChild} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {CommonModule} from "@angular/common";
 
@@ -9,6 +9,7 @@ import {CommonModule} from "@angular/common";
  * TensorFlow imports
  * **/
 import * as cocoSsd from '@tensorflow-models/coco-ssd';
+import {IStyles} from "./app.models";
 
 @Component({
   selector: 'app-root',
@@ -17,25 +18,26 @@ import * as cocoSsd from '@tensorflow-models/coco-ssd';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
+  private model!: cocoSsd.ObjectDetection;
   @ViewChild('webcam') webcamElementRef!: ElementRef;
   title = 'web-app';
-  model: any;
   hideButton= false;
   hideDemos = true;
-  children= [];
   predictionsLabel = '';
-  predictionsLabelStyles = {};
-  highlighterStyles= {};
+  predictionsLabelStyles: IStyles = { 'display' : 'none'};
+  highlighterStyles: IStyles= {};
 
-  async ngOnInit(){
-    // Before we can use COCO-SSD class we must wait for it to finish
-    // loading. Machine Learning models can be large and take a moment
-    // to get everything needed to run.
-    cocoSsd.load().then((loadedModel) => {
-      this.model = loadedModel;
-      // Show demo section now model is ready to use.
-      this.hideDemos = false;
+  constructor() {
+    effect(() => {
+      // Before we can use COCO-SSD class we must wait for it to finish
+      // loading. Machine Learning models can be large and take a moment
+      // to get everything needed to run.
+      cocoSsd.load().then((loadedModel: cocoSsd.ObjectDetection) => {
+        this.model = loadedModel;
+        // Show demo section now model is ready to use.
+        this.hideDemos = false;
+      });
     });
   }
   enableCam():void {
@@ -50,44 +52,47 @@ export class AppComponent implements OnInit {
       video: true
     };
     // Activate the webcam stream.
-    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-      this.webcamElementRef.nativeElement.srcObject = stream;
-      this.hideDemos = false;
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then((stream) => {
+        this.webcamElementRef.nativeElement.srcObject = stream;
+        this.hideDemos = false;
     });
   }
   predictWebcam(){
     // Now let's start classifying a frame in the stream.
-    this.model.detect(this.webcamElementRef.nativeElement).then( (predictions:any)=> {
-      // Now lets loop through predictions and draw them to the live view if
-      // they have a high confidence score.
-      predictions.forEach((prediction: any) => {
-        // If we are over 66% sure we are sure we classified it right, draw it!
-        if(prediction.score > 0.66){
-          this.predictionsLabel = `${prediction.class} - with ${Math.round(parseFloat(prediction.score) * 100)} % confidence.`;
-          const labelStyles = {
-            'margin-left': `${prediction.bbox[0]}px`,
-            'margin-top': `${prediction.bbox[1] - 10}px`,
-            'width': `${prediction.bbox[2] - 10}px`,
-            'top': '0px',
-            'left': '0px'
-          };
+    this.model.detect(this.webcamElementRef.nativeElement)
+      .then( (predictions:cocoSsd.DetectedObject[])=> {
+        // Now lets loop through predictions and draw them to the live view if
+        // they have a high confidence score.
+        predictions.forEach((prediction: cocoSsd.DetectedObject) => {
+          // If we are over 66% sure we are sure we classified it right, draw it!
+          if(prediction.score > 0.66){
+            this.predictionsLabel = `${prediction.class} - with ${Math.round(parseFloat(prediction.score.toString()) * 100)} % confidence.`;
+            const { bbox } = prediction;
+            const labelStyles = {
+              'margin-left': `${bbox[0]}px`,
+              'margin-top': `${bbox[1] - 10}px`,
+              'width': `${bbox[2] - 10}px`,
+              'top': '0px',
+              'left': '0px'
+            };
 
-          this.predictionsLabelStyles = {...labelStyles};
+            this.predictionsLabelStyles = {...labelStyles};
 
-          const highlighterStyles = {
-            'left': `${prediction.bbox[0]}px`,
-            'top': `${prediction.bbox[1]}px`,
-            'width': `${prediction.bbox[2]}px`,
-            'height': `${prediction.bbox[3]}px`
-          };
+            const highlighterStyles = {
+              'left': `${bbox[0]}px`,
+              'top': `${bbox[1]}px`,
+              'width': `${bbox[2]}px`,
+              'height': `${bbox[3]}px`
+            };
 
-          this.highlighterStyles = {...highlighterStyles};
+            this.highlighterStyles = {...highlighterStyles};
 
-        }
-      })
+          }
+        })
 
-      // Call this function again to keep predicting when the browser is ready.
-      window.requestAnimationFrame(() => this.predictWebcam());
+        // Call this function again to keep predicting when the browser is ready.
+        window.requestAnimationFrame(() => this.predictWebcam());
     });
   }
 }
